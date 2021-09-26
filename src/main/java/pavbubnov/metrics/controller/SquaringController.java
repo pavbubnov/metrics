8df99547a271c39013ef5a851444c2f70f8bdd77
pavbubnov <pavbubnov@yandex.ru>
2021-09-26 13:13:26 +0300

    изменения

--- src/main/java/pavbubnov/metrics/controller/SquaringController.java
+++ src/main/java/pavbubnov/metrics/controller/SquaringController.java
@@ -4,11 +4,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
 public class SquaringController {
 
     @GetMapping("/square/{number}")
-    public Double getSquare(@PathVariable("number") Double number) {
+    public Double getSquare(@PathVariable("number") Double number) throws InterruptedException {
+        TimeUnit.SECONDS.sleep(5);
         return Math.pow(number, 2);
     }
 
