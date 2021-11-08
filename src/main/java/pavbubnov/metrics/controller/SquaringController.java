d1c3b2e044604e31164ca23bfc9c3318f10cedbf
Бубнов Павел Александрович <PABubnov@sberbank.ru>
2021-11-08 17:13:38 +0300

    итоговый вариант

--- src/main/java/pavbubnov/metrics/controller/SquaringController.java
+++ src/main/java/pavbubnov/metrics/controller/SquaringController.java
@@ -3,16 +3,20 @@ package pavbubnov.metrics.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.concurrent.TimeUnit;
+import pavbubnov.metrics.service.SquareService;
 
 @RestController
 public class SquaringController {
 
+    private final SquareService squareService;
+
+    public SquaringController(SquareService squareService) {
+        this.squareService = squareService;
+    }
+
     @GetMapping("/square/{number}")
     public Double getSquare(@PathVariable("number") Double number) throws InterruptedException {
-        TimeUnit.SECONDS.sleep(5);
-        return Math.pow(number, 2);
+        return squareService.getSquare(number);
     }
 
 }
